db0abf3651b6a5f6abb0c89bd3d1427da745051e
Alex Chin <alex.ba.chin@gmail.com>
2012-04-18 11:20:28 -0400

    Update project.clj for lein2.

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject lein-mustache "0.2-SNAPSHOT"
   :description "Leiningen plugin for evaluating Mustache templates"
   :eval-in-leiningen true
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [de.ubercode.clostache/clostache "1.0.0"]]
-  :dev-dependencies [[midje "1.3.1"]
-                     [lein-midje "1.0.8"]])
+  :min-lein-version "2.0.0"
+  :dependencies [[de.ubercode.clostache/clostache "1.0.0"]]
+  :profiles {:dev {:resource-paths ["test-resources"]
+                   :dependencies [[midje "1.3.1"]]}}
+  :plugins [[lein-midje "2.0.0-SNAPSHOT"]])
